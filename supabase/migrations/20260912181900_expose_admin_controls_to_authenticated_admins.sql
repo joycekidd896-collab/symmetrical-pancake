4e3d87c6c05b776d3ca36ccb5720aac6fd24618e
Joyce Stanford <joycekidd896@gmail.com>
2026-09-12 13:18:28 -0500

    Correct admin migration filename to match applied version

--- supabase/migrations/20260912181900_expose_admin_controls_to_authenticated_admins.sql
+++
@@ -1,3 +0,0 @@
-grant execute on function public.admin_approve_merchant(uuid) to authenticated;
-grant execute on function public.admin_reject_merchant(uuid) to authenticated;
-grant execute on function public.admin_set_merchant_active(uuid,boolean) to authenticated;
